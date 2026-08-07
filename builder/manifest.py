0d6a88d670ceacce7bd20f9e7f15f080dc89580f
Rian <lucaguerian@outlook.com>
2026-08-06 23:25:07 -0300

    fix: builder/manifest.py

--- builder/manifest.py
+++ builder/manifest.py
@@ -2,12 +2,22 @@ import os
 import xml.etree.ElementTree as ET
 from builder.utils import log
 
+_NS = "http://schemas.android.com/apk/res/android"
+_NS_PREFIX = f"{{{_NS}}}"
+
+# Top-level elements merged by name attribute
+_NAMED_TOP = ("uses-permission", "uses-feature", "permission", "permission-group")
+# <application> children merged by android:name
+_NAMED_APP = ("activity", "service", "receiver", "provider")
+# <application> children merged by android:name (no dedup check needed)
+_UNNAMED_APP = ("meta-data",)
+
 
 def merge(config):
-    lib_manifests = []
     if not os.path.isdir(config.libs_dir):
         return
 
+    lib_manifests = []
     for root_dir, _, files in os.walk(config.libs_dir):
         for f in files:
             if f == "AndroidManifest.xml":
@@ -17,62 +27,56 @@ def merge(config):
         return
 
     log.info("Merging %d library manifests", len(lib_manifests))
+    ET.register_namespace("android", _NS)
+
     main_tree = ET.parse(config.manifest_path)
     main_root = main_tree.getroot()
-
-    ns = "http://schemas.android.com/apk/res/android"
-    ET.register_namespace("android", ns)
-
-    existing_perms = set()
-    for perm in main_root.findall("uses-permission"):
-        name = perm.attrib.get(f"{{{ns}}}name", "")
-        if name:
-            existing_perms.add(name)
-
-    existing_features = set()
-    for feat in main_root.findall("uses-feature"):
-        name = feat.attrib.get(f"{{{ns}}}name", "")
-        if name:
-            existing_features.add(name)
-
     main_app = main_root.find("application")
 
-    existing_components = set()
+    # Build dedup sets
+    existing_top = {}  # tag -> set of names
+    for tag in _NAMED_TOP:
+        existing_top[tag] = {
+            e.attrib.get(f"{_NS_PREFIX}name", "")
+            for e in main_root.findall(tag)
+        }
+
+    existing_app = {}  # tag -> set of names
     if main_app is not None:
-        for tag in ("activity", "service", "receiver", "provider"):
-            for comp in main_app.findall(tag):
-                name = comp.attrib.get(f"{{{ns}}}name", "")
-                if name:
-                    existing_components.add(name)
+        for tag in _NAMED_APP:
+            existing_app[tag] = {
+                e.attrib.get(f"{_NS_PREFIX}name", "")
+                for e in main_app.findall(tag)
+            }
 
     for manifest_path in lib_manifests:
         try:
-            lib_tree = ET.parse(manifest_path)
-            lib_root = lib_tree.getroot()
-
-            for perm in lib_root.findall("uses-permission"):
-                name = perm.attrib.get(f"{{{ns}}}name", "")
-                if name and name not in existing_perms:
-                    main_root.append(perm)
-                    existing_perms.add(name)
-                    log.debug("Merged permission: %s", name)
-
-            for feat in lib_root.findall("uses-feature"):
-                name = feat.attrib.get(f"{{{ns}}}name", "")
-                if name and name not in existing_features:
-                    main_root.append(feat)
-                    existing_features.add(name)
-                    log.debug("Merged feature: %s", name)
-
+            lib_root = ET.parse(manifest_path).getroot()
             lib_app = lib_root.find("application")
+
+            # Merge top-level named elements
+            for tag in _NAMED_TOP:
+                for elem in lib_root.findall(tag):
+                    name = elem.attrib.get(f"{_NS_PREFIX}name", "")
+                    if name not in existing_top[tag]:
+                        main_root.append(elem)
+                        existing_top[tag].add(name)
+                        log.debug("Merged <%s>: %s", tag, name)
+
+            # Merge <application> children
             if lib_app is not None and main_app is not None:
-                for tag in ("activity", "service", "receiver", "provider", "meta-data"):
-                    for comp in lib_app.findall(tag):
-                        name = comp.attrib.get(f"{{{ns}}}name", "")
-                        if name and name not in existing_components:
-                            main_app.append(comp)
-                            existing_components.add(name)
-                            log.debug("Merged %s: %s", tag, name)
+                for tag in _NAMED_APP:
+                    for elem in lib_app.findall(tag):
+                        name = elem.attrib.get(f"{_NS_PREFIX}name", "")
+                        if name not in existing_app.get(tag, set()):
+                            main_app.append(elem)
+                            existing_app.setdefault(tag, set()).add(name)
+                            log.debug("Merged <application/%s>: %s", tag, name)
+
+                for tag in _UNNAMED_APP:
+                    for elem in lib_app.findall(tag):
+                        if main_app is not None:
+                            main_app.append(elem)
 
         except ET.ParseError:
             log.warning("Failed to parse: %s", manifest_path)
