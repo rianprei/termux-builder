6aa591aa44405487207bfce632ef74339c2f4e4b
Rian <lucaguerian@outlook.com>
2026-08-07 11:02:28 -0300

    chore: bump to v3.4.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="termux-builder",
-    version="3.4.4",
+    version="3.4.5",
     description="Android Studio no Termux — build APK sem root, sem PC",
     author="rianprei",
     url="https://github.com/rianprei/termux-builder",
